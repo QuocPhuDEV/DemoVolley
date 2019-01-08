16dc64f74842f64df9478e822637222ffeff2f08
QuocPhuDEV <phuhq.it@gmail.com>
2019-01-08 11:50:35 +0700

    2th

--- app/src/main/java/com/example/hoangquocphu/demovolley/MainActivity.java
+++ app/src/main/java/com/example/hoangquocphu/demovolley/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.hoangquocphu.demovolley;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_JSonRequest:
+                startActivity(new Intent(MainActivity.this, JSonRequestActivity.class));
+                break;
+
+            case R.id.button_StringRequest:
+                startActivity(new Intent(MainActivity.this, StringRequestActivity.class));
+                break;
+            case R.id.button_ImageRequest:
+                startActivity(new Intent(MainActivity.this, ImageRequestActivity.class));
+                break;
+            default:
+                break;
+        }
+
     }
 }
